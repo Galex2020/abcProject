e1e8ec40c6f9115444b02a00cdd6ae55e39dbcf3
Alexander <G.Alex.Creeper@gmail.com>
2020-07-06 19:07:50 +0800

    Initial Commit

---
+++ abc123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  abc123
+//
+//  Created by G_Alex on 06.07.2020.
+//  Copyright © 2020 G_Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
